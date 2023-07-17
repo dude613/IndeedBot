3c97bfbf1008e78c36069273859f6ed0bc4e3ebc
collinsanele <collinsanele@gmail.com>
2023-07-17 18:35:13 +0100

    scrape all listings in all pages

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 from utils import calculate_sentence_similarity
 import pandas as pd
 import chromedriver_autoinstaller
@@ -25,6 +26,9 @@ RESULT_CONFIDENCE = []
 #Create array/list for job title for indeed
 RESULT_TITLE = []
 
+#Create array/list for position
+RESULT_POSITION = []
+
 #read csv file 
 df = pd.read_csv("IndeedBot.csv")
 
@@ -35,20 +39,30 @@ positions = df.iloc[:,0].tolist()
 
 driver = uc.Chrome(use_subprocess=True, options=opt) 
 
+
 #Loop through each position in positions list and search for it on indeed.com
 for position in positions:
     driver.get(SEARCH_URL.replace("searchQuery", position))
-    job_title = driver.find_element(by=By.CLASS_NAME, value='jobTitle').text.strip()
-    RESULT_TITLE.append(job_title)
-    confidence_level = calculate_sentence_similarity(position.lower(), job_title.lower())
-    RESULT_CONFIDENCE.append(confidence_level)
-
+    while True:
+        job_titles = driver.find_elements(by=By.CLASS_NAME, value='jobTitle')
+        for job_title in job_titles:
+            RESULT_POSITION.append(position)
+            RESULT_TITLE.append(job_title.text.strip())
+            confidence_level = calculate_sentence_similarity(position.lower(), job_title.text.lower())
+            RESULT_CONFIDENCE.append(confidence_level)
+        try:
+            next_btn = driver.find_element(by=By.CSS_SELECTOR, value='a[data-testid="pagination-page-next"]')
+            next_btn.click()
+        except Exception as e:
+            break
+        
 
 #Create final result dataframe
-df_result = pd.DataFrame({"Position": positions, "Title": RESULT_TITLE, "Confidence Level": RESULT_CONFIDENCE})
+df_result = pd.DataFrame({"Position": RESULT_POSITION, "Title": RESULT_TITLE, "Confidence Level": RESULT_CONFIDENCE})
 
 #print the first 20 rows
 print(df_result.head(20))
 
 #save final result as a csv file called result.csv
 df_result.to_csv("result.csv", index=False)
+
